93295c99687a81a1c35486b853a9beb89af7ca83
Rachel Peterson <rachelp5446@gmail.com>
2020-11-10 17:39:08 -0600

    second commit

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -16,3 +16,15 @@ module.exports.createProduct = (req, res) => {
         .then(product => res.json(product))
         .catch(err => res.json(err))
 }
+
+module.exports.getAllProducts = (req, res) => {
+    Product.find({})
+        .then(products => res.json(products))
+        .catch(err => res.json(err))
+}
+
+module.exports.getOneProduct = (req, res) => {
+    Product.findOne({_id:req.params.id})
+        .then(product => res.json(product))
+        .catch(err => res.json(err))
+}
