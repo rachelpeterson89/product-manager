9f8bdae34c43470141a40005a375dab56028c9ef
Rachel Peterson <rachelp5446@gmail.com>
2020-11-11 11:24:58 -0600

    third commit, added delete and update functions

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -4,5 +4,7 @@ module.exports = function(app) {
     app.get('/api', ProductController.index);
     app.post('/api/product', ProductController.createProduct);
     app.get('/api/product', ProductController.getAllProducts);
-    app.get('/api/product/:id', ProductController.getOneProduct)
+    app.get('/api/product/:id', ProductController.getOneProduct);
+    app.put('/api/product/:id', ProductController.updateProduct);
+    app.delete('/api/product/:id', ProductController.deleteProduct)
 }
